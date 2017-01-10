143a4107433848fb86c114a803a53cb1ad3bc4bb
Federico Ruiz <federicoruiz.ruiz@gmail.com>
2017-01-10 22:20:39 +0100

    Previo to Form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 /**
  * Created by sp29580 on 8/1/17.
  */
+
+
+$(window).on("load", function() {
+  $('#coverimage').find('img').each(function() {
+    var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
+    $(this).addClass(imgClass);
+  })
+});
+
+
+
 jQuery('a[href^="#"]').click(function (e) {
 
   jQuery('html,body').animate({scrollTop: jQuery(this.hash).offset().top}, 1000);
