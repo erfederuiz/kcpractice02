fd77ab676defc2462ea712b33e1976f74c75aeda
Federico Ruiz <federicoruiz.ruiz@gmail.com>
2017-01-12 22:03:41 +0100

    Formulario ok-1

--- js/script.js
+++ js/script.js
@@ -13,12 +13,44 @@ $(window).on("load", function() {
 
 
 jQuery('a[href^="#"]').click(function (e) {
-
   jQuery('html,body').animate({scrollTop: jQuery(this.hash).offset().top}, 1000);
-
   return false;
-
   e.preventDefault();
-
 });
 
+function validarEmail($emailContent) {
+  var emailReg = /^([\w.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailReg.test($emailContent);
+}
+
+function validacionFormulario(){
+// Storing Field Values In Variables
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var contact = document.getElementById("contact").value;
+// Regular Expression For Email
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+// Conditions
+  if (name != '' && email != '' && contact != '') {
+    if (email.match(emailReg)) {
+      if (document.getElementById("male").checked || document.getElementById("female").checked) {
+        if (contact.length == 10) {
+          alert("All type of validation has done on OnSubmit event.");
+          return true;
+        } else {
+          alert("The Contact No. must be at least 10 digit long!");
+          return false;
+        }
+      } else {
+        alert("You must select gender.....!");
+        return false;
+      }
+    } else {
+      alert("Invalid Email Address...!!!");
+      return false;
+    }
+  } else {
+    alert("All fields are required.....!");
+    return false;
+  }
+}
