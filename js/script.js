65167fc1a36f87401f9c1e9418efcac4139415d8
Federico Ruiz <federicoruiz.ruiz@gmail.com>
2017-01-14 02:17:12 +0100

    Ajax tasks 01

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  * Created by sp29580 on 8/1/17.
  */
 
-
+//FUNCIONES DESPLAZAMIENTO Y FORMATO
 $(window).on("load", function() {
   $('#coverimage').find('img').each(function() {
     var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
@@ -11,46 +11,99 @@ $(window).on("load", function() {
 });
 
 
-
 jQuery('a[href^="#"]').click(function (e) {
   jQuery('html,body').animate({scrollTop: jQuery(this.hash).offset().top}, 1000);
   return false;
   e.preventDefault();
 });
 
-function validarEmail($emailContent) {
-  var emailReg = /^([\w.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-  return emailReg.test($emailContent);
-}
+// FUNCIONES VALIDACION FORMULARIO
+function validarCadena($textoValidar, $regex) {
+  var patronUsar = $regex ;
+  var resultado = patronUsar.test($textoValidar);
+  return resultado;
+};
 
-function validacionFormulario(){
-// Storing Field Values In Variables
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var contact = document.getElementById("contact").value;
-// Regular Expression For Email
-  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-// Conditions
-  if (name != '' && email != '' && contact != '') {
-    if (email.match(emailReg)) {
-      if (document.getElementById("male").checked || document.getElementById("female").checked) {
-        if (contact.length == 10) {
-          alert("All type of validation has done on OnSubmit event.");
-          return true;
-        } else {
-          alert("The Contact No. must be at least 10 digit long!");
-          return false;
-        }
-      } else {
-        alert("You must select gender.....!");
-        return false;
+function contarPalabras(){
+  //document.getElementById("textoEscondido").value
+  // var palabras = $('#').val().split(/\b[\s,\.\-:;]*/).lenght;
+  var texto = document.getElementById("textoEscondido").value;
+  var palabras = texto.split(/\b[\s,\.\-:;]*/).length;
+  return palabras;
+};
+
+function validacionFormulario() {
+  var nombre = document.getElementById("nombreForm").value;
+  var apellido = document.getElementById("apellidoForm").value;
+  var email = document.getElementById("emailForm").value;
+  var phone = document.getElementById("phoneForm").value;
+
+  var regexEmail = /^[\w.-_]+@{1}[\w]+\.[a-z]{2,3}$/;
+  var regexPhone1 = /^\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/;
+  var regexPhone2 = /^(6|7|8|9)[0-9]{8}$/;
+
+// Comprobaciones
+  if (nombre == "" || apellido == "" ||  email == "" || phone == "") {
+    alert("Todos los campos son obligatorios.....!");
+    return false;
+  }else{
+
+    var estadoEmail = validarCadena(email, regexEmail);
+    var estadoPhone = validarCadena(phone, regexPhone1 ) ||
+                      validarCadena(phone, regexPhone2  ) ? true : false ;
+
+    var estadoConocimiento =  document.getElementById("rd1").checked ||
+                              document.getElementById("rd2").checked ||
+                             (document.getElementById("rd3").checked && contarPalabras() < 150 ) ? true : false ;
+
+    if( estadoEmail && estadoPhone && estadoConocimiento){
+      return true;
+    }else{
+      var mensaje = "Errores al validar:\n"
+      if (!estadoEmail){
+        mensaje = mensaje + "El formato del email no es correcto!\n";
       }
-    } else {
-      alert("Invalid Email Address...!!!");
+      if (!estadoPhone){
+        mensaje = mensaje + "El formato del teléfono no es correcto!\n";
+      }
+      if (!estadoConocimiento && document.getElementById("rd3").checked ){
+        mensaje = mensaje + "Escribe menos de 150 palabros!";
+      }
+      alert(mensaje);
       return false;
     }
-  } else {
-    alert("All fields are required.....!");
-    return false;
+
   }
+}
+
+//FUNCIONES AJAX TO-DO LIST
+
+
+
+/*
+var itemTemplate = $('#templates .item')
+var list         = $('#list')
+
+ var addItemToPage = function(itemData) {
+ var item = itemTemplate.clone()
+ item.attr('data-id',itemData.id)
+ item.find('.description').text(itemData.description)
+ if(itemData.completed) {
+ item.addClass('completed')
+ }
+ list.append(item)
+ }
+*/
+
+var plantillaElementos = $('#plantillaTarea .tarea');
+var listaTareas         = $('#listaTareas');
+
+var insertarElementoLista = function(datosElemento) {
+  var elemento = plantillaElementos.clone()
+  elemento.attr('data-id',datosElemento.id)
+  elemento.find('.description').text(datosElemento.description)
+  if(datosElemento.completed) {
+    elemento.addClass('completed')
+  }
+  listaTareas.append(elemento)
 }
